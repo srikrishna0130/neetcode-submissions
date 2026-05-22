8ef1781d37ca985bfe73d8a757a3bb809e763333
Srikrishna Gandluru <srikrishna0130@gmail.com>
2026-05-22 18:52:00 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visited = set()
+        current_path = set()
+        adj = defaultdict(list)
+
+        for a, b in prerequisites:
+            adj[b].append(a)
+        
+        def dfs(current):
+            next_courses = adj[current]
+
+            for nc in next_courses:
+                if nc in visited:
+                    continue
+                if nc in current_path:
+                    return False
+                current_path.add(nc)
+                if not dfs(nc):
+                    return False
+                current_path.remove(nc)
+                visited.add(nc)
+            
+            return True
+
+
+        for i in range(numCourses):
+            if i not in visited:
+                current_path.add(i)
+                if not dfs(i):
+                    return False
+                current_path.remove(i)
+                visited.add(i)
+        
+        return True
