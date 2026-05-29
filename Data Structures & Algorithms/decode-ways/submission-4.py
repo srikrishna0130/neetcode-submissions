90c6acc741a6175b43211b0f56feba8d11189e00
Srikrishna Gandluru <srikrishna0130@gmail.com>
2026-05-29 13:58:29 -0400

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+
+        dp = [0]*len(s)
+        for i in range(len(s)):
+            if i == 0:
+                dp[i] = 1
+                continue
+            
+            if s[i-1] == s[i] == '0':
+                return 0
+
+            # previous set with the current letter
+            if s[i] != '0':
+                dp[i] = dp[i-1]
+            # condition to check if there can be a merge possible
+            if '10' <= s[i-1]+s[i] <= '26':
+                if i == 1:
+                    dp[i] += 1
+                else:
+                    dp[i] += dp[i-2]
+
+            if dp[i] == 0:
+                return 0
+
+        return dp[-1]
+            
+            
+
