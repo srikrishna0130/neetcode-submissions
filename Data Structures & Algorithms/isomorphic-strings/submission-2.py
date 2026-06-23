2e9f311d0d7c1363ae78ce981526a7e8b714f651
Srikrishna Gandluru <srikrishna0130@gmail.com>
2026-06-23 06:38:58 -0400

    Add: isomorphic-strings - submission-2

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+
+        for i in range(len(s)):
+            if s[i] in s_map and (s_map[s[i]] != t[i]):
+                return False
+            if t[i] in t_map and (t_map[t[i]] != s[i]):
+                return False
+
+            s_map[s[i]] = t[i]
+            t_map[t[i]] = s[i]
+        
+        return True
