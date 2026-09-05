a94fd99528eb9f46d21d0c82b92cb624d8292101
Srikrishna Gandluru <srikrishna0130@gmail.com>
2026-09-05 02:23:03 -0700

    Add: cheapest-flight-path - submission-5

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-5.py
@@ -0,0 +1,26 @@
+"""
+will try the bellmand ford algo with a queue over here for better traversal
+"""
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = defaultdict(list)
+        min_cost = 10**6+1
+        for s, d, cost in flights:
+            adj[s].append([d, cost])
+        
+        min_distances = [min_cost]*n
+        min_distances[src] = 0
+        bfs = deque([(src, 0, 0)])
+
+        while bfs:
+            city, cost, stops= bfs.popleft()
+            if stops > k:
+                break
+            
+            for nei, nei_cost in adj[city]:
+                if min_distances[nei] > cost + nei_cost:
+                    min_distances[nei] = cost + nei_cost
+                    bfs.append((nei, cost + nei_cost, stops + 1))
+            
+        return -1 if min_distances[dst] == 1000001 else min_distances[dst]
+       
